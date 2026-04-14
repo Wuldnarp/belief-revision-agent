0c146fe8f565baa435a9bf7de26c1e4aa10711b4
Emre Eker <EmreEker@outlook.dk>
2026-04-14 09:37:40 +0200

    first commit

---
+++ src/main/java/org/dtu/introai/App.java
@@ -0,0 +1,13 @@
+package org.dtu.introai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
